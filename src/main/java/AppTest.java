ef4113c29fc2e738e0b069006a832ca1a721c2ed
xw <3196887235@qq.com>
2022-07-19 15:33:17 +0800

    delete app,AppTest.java

--- src/main/java/AppTest.java
+++
@@ -1,7 +0,0 @@
-/**
- * Description:
- * Author: wxy
- * Date: 2022/7/18 18:28
- */
-public class AppTest {
-}
--- src/main/java/app.java
+++
@@ -1,9 +0,0 @@
-/**
- * Description:
- * Author: wxy
- * Date: 2022/7/18 17:41
- * 11
- github master modifed
- */
-public class app {
-}
